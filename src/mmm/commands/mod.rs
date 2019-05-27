cb4bd69a543493d2c36d0476ee0d93633e9a2049
Daniel Duan <daniel@duan.ca>
2019-05-27 15:20:40 -0700

    Add open command

--- src/mmm/commands/mod.rs
+++ src/mmm/commands/mod.rs
@@ -1,9 +1,11 @@
 mod edit_command;
+mod open_command;
 
 use super::Command;
 
 pub fn all_commands() -> Vec<Box<Command>> {
     return vec![
         Box::new(edit_command::EditCommand {}),
+        Box::new(open_command::OpenCommand {}),
     ];
 }
---
+++ src/mmm/commands/open_command.rs
@@ -0,0 +1,18 @@
+use crate::mmm::Command;
+use open;
+use std::path::Path;
+
+pub struct OpenCommand {}
+
+impl Command for OpenCommand {
+    fn name(&self) -> String { String::from("Open") }
+    fn hotkey_pos(&self) -> usize { 0 }
+    fn exe_msg(&self, path: &String) -> Option<String> { Some(format!("{} {}\n", "Opening", path)) }
+
+    fn should_show(&self, path: &String) -> bool { Path::new(path).exists() }
+
+    #[allow(unused_variables)]
+    fn execute(&self, path: &String, followup_input: Option<String>) -> bool {
+        return open::that(path).is_ok();
+    }
+}
