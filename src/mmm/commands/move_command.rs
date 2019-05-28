9987bfe137f67cae5632229c2b83b9cad820d32a
Daniel Duan <daniel@duan.ca>
2019-05-27 18:47:49 -0700

    Support moving to folder

--- src/mmm/commands/move_command.rs
+++ src/mmm/commands/move_command.rs
@@ -1,15 +1,23 @@
 use crate::mmm::Command;
+use std::fmt::Display;
 use crate::mmm::utils;
 use std::fs;
 use std::path::Path;
+use termion::color;
 
 pub struct MoveCommand {}
+
+impl MoveCommand {
+    fn log_move_attempt<T>(&self, msg: T) where T: Display {
+        utils::log(format!("Moving to {}\n", utils::color_text(msg, color::Yellow)));
+    }
+}
 
 impl Command for MoveCommand {
     fn name(&self) -> String { String::from("Move") }
     fn hotkey_pos(&self) -> usize { 0 }
     fn exe_msg(&self, path: &String) -> Option<String> {
-        Some(format!("{} {}\n", "Moving", path))
+        Some(format!("{} {}\n", "Moving   ", path))
     }
 
     fn should_show_if_path_exists_not(&self) -> bool { false }
@@ -31,6 +39,21 @@ impl Command for MoveCommand {
         }
 
         let dest_path = Path::new(&destination);
+        let dest_is_dir = dest_path.is_dir();
+
+        if destination.ends_with(std::path::MAIN_SEPARATOR) && !dest_is_dir {
+            utils::elog(format!(
+                "I can't move it to {}, that place doesn't exist yet!\n",
+                dest_path.to_str().unwrap()));
+            return false
+        }
+
+        if dest_is_dir {
+            let new_dest = dest_path.join(Path::new(path).file_name().unwrap());
+            self.log_move_attempt(new_dest.to_str().unwrap());
+            return fs::rename(path, new_dest).is_ok()
+        }
+
         match dest_path.parent() {
             None => false,
             Some(parent_path) => {
@@ -40,9 +63,14 @@ impl Command for MoveCommand {
                         parent_path.to_str().unwrap()));
                     false
                 } else {
+                    self.log_move_attempt(&destination);
                     fs::rename(path, destination).is_ok()
                 }
             }
         }
+    }
+
+    fn wrapup_msg(&self) -> String {
+        String::from("Done!\n")
     }
 }
--- src/mmm/utils.rs
+++ src/mmm/utils.rs
@@ -1,11 +1,11 @@
 use atty;
-use std::fmt;
+use std::fmt::Display;
 use std::io::Write;
 use std::io;
 use termion::{color, style};
 
 pub fn color_text<T:, C>(text: T, text_color: C) -> String
-    where T: fmt::Display, C: color::Color
+    where T: Display, C: color::Color
 {
     if atty::is(atty::Stream::Stdout) {
         return format!("{}{}{}", color::Fg(text_color), text, style::Reset);
@@ -14,13 +14,13 @@ pub fn color_text<T:, C>(text: T, text_color: C) -> String
     }
 }
 
-pub fn log<T>(log_text: T) where T: fmt::Display {
+pub fn log<T>(log_text: T) where T: Display {
     let mmm_success = color_text("mmm", color::Green);
     print!("[{}] {}", mmm_success, log_text);
     io::stdout().flush().expect("Flushing failed");
 }
 
-pub fn elog<T>(log_text: T) where T: fmt::Display {
+pub fn elog<T>(log_text: T) where T: Display {
     let mmm_warning = color_text("mmm", color::Yellow);
     eprint!("[{}] {}", mmm_warning, log_text);
     io::stderr().flush().expect("Flushing failed");
