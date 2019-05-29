511f9ca21cc18586b83035ae09c74a2a344bca41
Daniel Duan <daniel@duan.ca>
2019-05-29 09:08:34 -0700

    Hide edit command if no $EDITOR is set
    close #6.

--- src/mmm/commands/edit_command.rs
+++ src/mmm/commands/edit_command.rs
@@ -1,12 +1,21 @@
 use crate::mmm::Command;
-use crate::mmm::utils;
 use std::env;
 use std::process;
 
-pub struct EditCommand {}
+pub struct EditCommand {
+    editor: Option<String>
+}
 
 impl Command for EditCommand {
-    fn new() -> EditCommand { EditCommand {} }
+    fn new() -> EditCommand {
+        EditCommand {
+            editor: env::var("EDITOR").ok()
+        }
+    }
+
+    fn should_show_if_path_exists(&self) -> bool { self.editor.is_some() }
+    fn should_show_if_path_exists_not(&self) -> bool { self.editor.is_some() }
+
     fn name(&self) -> String { String::from("Edit") }
     fn hotkey_pos(&self) -> usize { 0 }
     fn exe_msg(&self, path: &String) -> Option<String> { Some(format!("{} {}\n", "Editing", path)) }
@@ -15,13 +24,7 @@ impl Command for EditCommand {
 
     #[allow(unused_variables)]
     fn execute(&self, path: &String, followup_input: Option<String>) -> bool {
-        let editor = env::var("EDITOR");
-        if editor.is_err() {
-            utils::elog("$EDITOR not set\n");
-            return false;
-        }
-
-        let mut editor_command = process::Command::new(format!("{}", editor.unwrap()));
+        let mut editor_command = process::Command::new(format!("{}", self.editor.as_ref().unwrap()));
         editor_command.arg(path);
         editor_command
             .spawn()
